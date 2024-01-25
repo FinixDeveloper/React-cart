23fbeb81903af22c4180e6e7a3bb673ab6df3209
FinixDeveloper <praveenkumarfinix@gmail.com>
2024-01-25 10:12:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import DataContextComponents from './Utils/DataContextComponents';
+import Card from './Components/Card';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <DataContextComponents>
+          <Card />
+        </DataContextComponents>
+      </div>
+    </div>
+  );
+}
+
+export default App;
